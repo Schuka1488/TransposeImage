9b43f9b9cfdc806dd4989790ee7b5a1fd601a092
Максим Щукин <oleg-14317@mail.ru>
2023-11-23 16:58:45 +0500

    Shipment #2

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 from PIL import Image
 import os
+import concurrent.futures
+
+def mirror_image(image_path):
+    try:
+        original_image = Image.open(image_path)
+        mirrored_image = original_image.transpose(Image.FLIP_LEFT_RIGHT)
+        new_filename = f"копия_{os.path.basename(image_path)}"
+        new_image_path = os.path.join(os.path.dirname(image_path), new_filename)
+        mirrored_image.save(new_image_path)
+    except IOError:
+        print(f"Ошибка. Не удается открыть изображение: {image_path}")
 
 def mirror_images_in_directory(directory):
-    for filename in os.listdir(directory):  # Перебираем файлы в указанной директории
-        if filename.endswith(".jpg") or filename.endswith(".png"):
-            image_path = os.path.join(directory, filename)  # Получаем путь к изображению
-            original_image = Image.open(image_path)  # Открываем изображение
-            mirrored_image = original_image.transpose(Image.FLIP_LEFT_RIGHT)  # Отзеркаливаем изображение
-            new_filename = f"копия_{filename}"  # Создаем новое имя файла
-            new_image_path = os.path.join(directory, new_filename)
-            mirrored_image.save(new_image_path)  # Сохраняем отраженную копию в указанной директории
+    image_paths = [os.path.join(directory, filename) for filename in os.listdir(directory)
+                   if filename.endswith(".jpg") or filename.endswith(".png") or
+                   filename.endswith(".jpeg") or filename.endswith(".gif") or
+                   filename.endswith(".bmp")]
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.map(mirror_image, image_paths)
 
 if __name__ == "__main__":
     directory_path = "D:/PythonProject/PythonImage"
     mirror_images_in_directory(directory_path)
-
